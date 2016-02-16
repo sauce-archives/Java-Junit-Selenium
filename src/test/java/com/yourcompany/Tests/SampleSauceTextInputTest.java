06e521a9ec047628bacdf4be3006edbab41cdd12
jsmoxon <jsmoxon@gmail.com>
2016-02-16 14:25:51 -0800

    Update SampleSauceTextInputTest.java

--- src/test/java/com/yourcompany/Tests/SampleSauceTextInputTest.java
+++ src/test/java/com/yourcompany/Tests/SampleSauceTextInputTest.java
@@ -29,7 +29,8 @@ public class SampleSauceTextInputTest extends SampleSauceTestBase {
      */
     @Test
     public void verifyEmailInputTest() throws InvalidElementStateException {
-        String emailInputText = "abc@gmail.com";
+        String emailInputText = "wrongemail@gmail.com";
+        String emailInputText2 = "abc@gmail.com";
 
         // Navigate to the page
         driver.get("https://saucelabs.com/test/guinea-pig");
@@ -47,7 +48,7 @@ public class SampleSauceTextInputTest extends SampleSauceTestBase {
          Assertions should be part of test and not part of Page object.
          Each test should be verifying one piece of functionality (atomic testing)
         */
-        assertEquals(page.getEmailText(), emailInputText);
+        assertEquals(page.getEmailText(), emailInputText2);
 
     }
 
